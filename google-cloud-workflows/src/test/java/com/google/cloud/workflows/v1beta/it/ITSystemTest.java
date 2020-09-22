6478b34e0a5b757bcf9c68687f0cfd8cf5fe0c0e
twinkle-kadia <twinkle.kadia@qlogic.io>
2020-09-22 10:24:39 +0530

    test: modify integration test

--- google-cloud-workflows/src/test/java/com/google/cloud/workflows/v1beta/it/ITSystemTest.java
+++ google-cloud-workflows/src/test/java/com/google/cloud/workflows/v1beta/it/ITSystemTest.java
@@ -16,77 +16,121 @@
 
 package com.google.cloud.workflows.v1beta.it;
 
-import com.google.api.gax.core.NoCredentialsProvider;
-import com.google.api.gax.grpc.GaxGrpcProperties;
-import com.google.api.gax.grpc.testing.LocalChannelProvider;
-import com.google.api.gax.grpc.testing.MockServiceHelper;
-import com.google.api.gax.rpc.ApiClientHeaderProvider;
-import com.google.cloud.workflows.v1beta.*;
-import com.google.protobuf.AbstractMessage;
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.workflows.v1beta.CreateWorkflowRequest;
+import com.google.cloud.workflows.v1beta.DeleteWorkflowRequest;
+import com.google.cloud.workflows.v1beta.ListWorkflowsRequest;
+import com.google.cloud.workflows.v1beta.LocationName;
+import com.google.cloud.workflows.v1beta.UpdateWorkflowRequest;
+import com.google.cloud.workflows.v1beta.Workflow;
+import com.google.cloud.workflows.v1beta.WorkflowName;
+import com.google.cloud.workflows.v1beta.WorkflowsClient;
+import com.google.protobuf.FieldMask;
+import com.google.protobuf.util.FieldMaskUtil;
+import java.io.IOException;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ITSystemTest {
-  private static MockWorkflows mockWorkflows;
-  private static MockServiceHelper serviceHelper;
+
+  private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
+  private static final String LOCATION = "us-central1";
+  private static final String ID = UUID.randomUUID().toString().substring(0, 8);
+  private static final String WORKFLOW_ID = "test-workflow-id-" + ID;
+  private static final String PARENT = LocationName.of(PROJECT_ID, LOCATION).toString();
+  private static final String WORKFLOW_NAME =
+      WorkflowName.of(PROJECT_ID, LOCATION, WORKFLOW_ID).toString();
+  private static final String WORKFLOW_DESCRIPTION = "workflow-test-description";
+  private static final String SOURCE_CONTENT =
+      "   # This is a sample workflow, feel free to replace it with your source code  "
+          + "   #  "
+          + "   # This workflow does the following:  "
+          + "   # - reads current time and date information from an external API and stores  "
+          + "   #   the response in CurrentDateTime variable  "
+          + "   # - retrieves a list of Wikipedia articles related to the day of the week  "
+          + "   #   from CurrentDateTime  "
+          + "   # - returns the list of articles as an output of the workflow  "
+          + "     "
+          + "   - getCurrentTime:  "
+          + "       call: http.get  "
+          + "       args:  "
+          + "           url: https://us-central1-workflowsample.cloudfunctions.net/datetime  "
+          + "       result: CurrentDateTime  "
+          + "   - readWikipedia:  "
+          + "       call: http.get  "
+          + "       args:  "
+          + "           url: https://en.wikipedia.org/w/api.php  "
+          + "           query:  "
+          + "               action: opensearch  "
+          + "               search: ${CurrentDateTime.body.dayOfTheWeek}  "
+          + "       result: WikiResult  "
+          + "   - returnOutput:  "
+          + "      return: ${WikiResult.body[1]}  ";
+  //      "# This is a sample workflow, feel free to replace it with your source code\n#\n# This
+  // workflow does the following:\n# - reads current time and date information from an external API
+  // and stores\n#   the response in CurrentDateTime variable\n# - retrieves a list of Wikipedia
+  // articles related to the day of the week\n#   from CurrentDateTime\n# - returns the list of
+  // articles as an output of the workflow\n\n- getCurrentTime:\n    call: http.get\n    args:\n
+  //    url: https://us-central1-workflowsample.cloudfunctions.net/datetime\n    result:
+  // CurrentDateTime\n- readWikipedia:\n    call: http.get\n    args:\n        url:
+  // https://en.wikipedia.org/w/api.php\n        query:\n            action: opensearch\n
+  // search: ${CurrentDateTime.body.dayOfTheWeek}\n    result: WikiResult\n- returnOutput:\n
+  // return: ${WikiResult.body[1]}\n";
   private static WorkflowsClient client;
-  private static LocalChannelProvider channelProvider;
-  private static final String LOCATION = "us-central1-a";
-  private static final String WORKFLOW_ID =
-      "test-workflow-" + UUID.randomUUID().toString().substring(0, 8);
+  private static Workflow expectedWorkflow;
 
   @BeforeClass
-  public static void beforeClass() throws Exception {
-    /** create workflow */
-    serviceHelper.reset();
-    channelProvider = serviceHelper.createChannelProvider();
-    WorkflowsSettings settings =
-        WorkflowsSettings.newBuilder()
-            .setTransportChannelProvider(channelProvider)
-            .setCredentialsProvider(NoCredentialsProvider.create())
+  public static void setUp() throws IOException, ExecutionException, InterruptedException {
+    // Create Test Workflow
+    client = WorkflowsClient.create();
+    Workflow workflow =
+        Workflow.newBuilder().setName(WORKFLOW_NAME).setSourceContents(SOURCE_CONTENT).build();
+    CreateWorkflowRequest request =
+        CreateWorkflowRequest.newBuilder()
+            .setParent(PARENT)
+            .setWorkflow(workflow)
+            .setWorkflowId(WORKFLOW_ID)
             .build();
-    client = WorkflowsClient.create(settings);
-  }
-
-  @Test
-  public void getWorkflowTest() {
-    WorkflowName name2 = WorkflowName.of("[PROJECT]", "[LOCATION]", "[WORKFLOW]");
-    String description = "description-1724546052";
-    String revisionId = "revisionId513861631";
-    String serviceAccount = "serviceAccount-1948028253";
-    String sourceContents = "sourceContents-1799875906";
-    Workflow expectedResponse =
-        Workflow.newBuilder()
-            .setName(name2.toString())
-            .setDescription(description)
-            .setRevisionId(revisionId)
-            .setServiceAccount(serviceAccount)
-            .setSourceContents(sourceContents)
-            .build();
-    mockWorkflows.addResponse(expectedResponse);
-
-    WorkflowName name = WorkflowName.of("[PROJECT]", "[LOCATION]", "[WORKFLOW]");
-
-    Workflow actualResponse = client.getWorkflow(name);
-    Assert.assertEquals(expectedResponse, actualResponse);
-
-    List<AbstractMessage> actualRequests = mockWorkflows.getRequests();
-    Assert.assertEquals(1, actualRequests.size());
-    GetWorkflowRequest actualRequest = (GetWorkflowRequest) actualRequests.get(0);
-
-    Assert.assertEquals(name, WorkflowName.parse(actualRequest.getName()));
-    Assert.assertTrue(
-        channelProvider.isHeaderSent(
-            ApiClientHeaderProvider.getDefaultApiClientHeaderKey(),
-            GaxGrpcProperties.getDefaultApiClientHeaderPattern()));
+    expectedWorkflow = client.createWorkflowAsync(request).get();
   }
 
   @AfterClass
-  public static void afterClass() throws Exception {
+  public static void tearDown() {
+    // Delete Test Worflow
+    DeleteWorkflowRequest deleteRequest =
+        DeleteWorkflowRequest.newBuilder().setName(WORKFLOW_NAME).build();
+    client.deleteWorkflowAsync(deleteRequest);
     client.close();
+  }
+
+  @Test
+  public void testGetWorkflow() {
+    Workflow workflow = client.getWorkflow(WORKFLOW_NAME);
+    assertEquals(expectedWorkflow, workflow);
+  }
+
+  @Test
+  public void testUpdateWorkflow() throws ExecutionException, InterruptedException {
+    Workflow workflow =
+        Workflow.newBuilder().setName(WORKFLOW_NAME).setDescription(WORKFLOW_DESCRIPTION).build();
+    FieldMask updateField = FieldMaskUtil.fromString("description");
+    UpdateWorkflowRequest request =
+        UpdateWorkflowRequest.newBuilder().setWorkflow(workflow).setUpdateMask(updateField).build();
+    Workflow updateWorkflow = client.updateWorkflowAsync(request).get();
+    assertEquals(expectedWorkflow, updateWorkflow);
+  }
+
+  @Test
+  public void testListWorkflows() {
+    ListWorkflowsRequest request = ListWorkflowsRequest.newBuilder().setParent(PARENT).build();
+    for (Workflow actualWorkflow : client.listWorkflows(request).iterateAll()) {
+      assertEquals(WORKFLOW_NAME, actualWorkflow.getName());
+      assertEquals(expectedWorkflow, actualWorkflow);
+    }
   }
 }
